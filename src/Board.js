df2dcee5e62b19450d2f83cb412d27e7dc00ae87
Casey Wittrock <caseywittrockmn@gmail.com>
2023-09-20 03:10:53 -0500

    finish assignment. update styles.

--- src/Board.js
+++ src/Board.js
@@ -27,22 +27,34 @@ import "./Board.css";
  *
  **/
 
-function Board({ nrows, ncols, chanceLightStartsOn }) {
+function Board({ nrows = 5, ncols = 5, chanceLightStartsOn = 0.25 }) {
   const [board, setBoard] = useState(createBoard());
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard() {
     let initialBoard = [];
+    for (let i = 0; i < nrows; i++) {
+      let row = [];
+      for (let j = 0; j < ncols; j++) {
+        row.push(Math.random() < chanceLightStartsOn);
+      }
+      initialBoard.push(row);
+    }
     // TODO: create array-of-arrays of true/false values
     return initialBoard;
   }
 
   function hasWon() {
+    if (board.every((row) => row.every((cell) => !cell))) {
+      return true;
+    }
+    return false;
+
     // TODO: check the board in state to determine whether the player has won.
   }
 
   function flipCellsAround(coord) {
-    setBoard(oldBoard => {
+    setBoard((oldBoard) => {
       const [y, x] = coord.split("-").map(Number);
 
       const flipCell = (y, x, boardCopy) => {
@@ -53,6 +65,16 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
         }
       };
 
+      const boardCopy = oldBoard.map((row) => [...row]);
+
+      flipCell(y, x, boardCopy);
+      flipCell(y, x - 1, boardCopy);
+      flipCell(y, x + 1, boardCopy);
+      flipCell(y - 1, x, boardCopy);
+      flipCell(y + 1, x, boardCopy);
+
+      return boardCopy;
+
       // TODO: Make a (deep) copy of the oldBoard
 
       // TODO: in the copy, flip this cell and the cells around it
@@ -60,6 +82,28 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
       // TODO: return the copy
     });
   }
+
+  if (hasWon()) {
+    return <div>You Won!</div>;
+  }
+
+  return (
+    <table className="Board">
+      <tbody>
+        {board.map((row, y) => (
+          <tr key={y}>
+            {row.map((cell, x) => (
+              <Cell
+                key={`${y}-${x}`}
+                isLit={cell}
+                flipCellsAroundMe={() => flipCellsAround(`${y}-${x}`)}
+              />
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 
   // if the game is won, just show a winning msg & render nothing else
 
